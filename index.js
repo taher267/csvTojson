182a296cc650c40759ab78011f23e56ce14d912b
ABU TAHER <abutaher267@gmail.com>
2024-07-01 18:51:11 +0600

    change funstion

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const fs = require("fs");
-const path = require("path");
-const repaceDblBtnComma = (input) => {
-    const regex = /"[^"]*"/g;
-    const found = input.match(regex);
-    let singleCopy = input;
-    for (let pick of found || []) {
-        let commaReplace = pick.replace(/,/g, "######");
-        const replacedBy = commaReplace.replace(/"/g, "");
-        singleCopy = singleCopy.replace(pick, replacedBy);
-    }
-    singleCopy = singleCopy.replace(/(~~~~~~~~)/g, '"');
-    return singleCopy;
-};
 const main = ({ fileSource, csv }) => {
-    if (!csv && fileSource) {
-        const filepath = path.resolve(fileSource);
-        csv = fs.readFileSync(filepath)?.toString?.();
-    }
-    const array = csv.toString().split(/\r\n|\n\r/); //|\n
-    let header = repaceDblBtnComma(array[0]).split(",");
-    let result = [];
-    for (const im of array.splice(1)) {
-        let DblQuoteReplace = im.replace(/("")/g, "~~~~~~~~");
-        const item = repaceDblBtnComma(DblQuoteReplace)
-            .split(",")
-            .map((item) => item.replace(/(######)/g, ","));
-        if (item?.join?.("")?.length) {
-            const obj = {};
-            for (const k in header) {
-                obj[header[k]] = item?.[k];
-            }
-            if (Object.keys(obj)?.length) {
-                result.push(obj);
-            }
+  const lines = csv.trim().split("\n");
+  headers = headers
+    ? headers
+    : lines[0].split(",").map((header) => header.trim());
+  if (typeof headers === "string") {
+    headers = headers.split(",").map((header) => header.trim());
+  }
+  const result = [];
+  let i = headers ? 0 : 1;
+  for (i; i < lines.length; i++) {
+    const obj = {};
+    let currentline = lines[i];
+    let j = 0;
+
+    // Split by commas not within quotes
+    currentline = currentline
+      .split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+      .map((value) => {
+        value = value.trim();
+        // Remove surrounding quotes and unescape double quotes
+        if (value.startsWith('"') && value.endsWith('"')) {
+          value = value.slice(1, -1).replace(/""/g, '"');
         }
-    }
-    return {
-        header,
-        data: result,
-    };
+        return value;
+      });
+
+    headers.forEach((header) => {
+      obj[header] = currentline[j++];
+    });
+
+    result.push(obj);
+  }
+
+  return {
+    headers,
+    data: result,
+  };
 };
 
 // console.log(main({ fileSource: "../../csv.csv" }));
